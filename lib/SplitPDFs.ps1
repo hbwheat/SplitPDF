694c33398b5720118e36690eff4383fbb1c91d6d
Ben <hbwheat@users.noreply.github.com>
2019-04-07 09:53:08 -0400

    Update SplitPDFs.ps1

--- lib/SplitPDFs.ps1
+++ lib/SplitPDFs.ps1
@@ -1,42 +1,43 @@
+#DEV ON JESTER Version 1.0.2
+
 #Change Log
 <#
-    2017.4.20 - Added "$pdfname-Extracted" to output file name to avoid naming conflicts
+    2014.4.26 - Added support for multiple leves of bookmarks
+                Adjusted display when a page is "guessed" to only show when actually duplicated
+                Added slight twist on control structre to not use page lables with numerics only. ie if page 1 -eq 1, to not use it
+    2014.4.25 - Added first ability to use bookmarks not page labels
+    2014.4.20 - Added "$pdfname-Extracted" to output file name to avoid naming conflicts
     2017.4.19 - Added exit if no page labels exist 
     
 #>
 #Description
 <#
+    Author: Ben Wheat
+    Date:   2017.04.25
+
     This script uses the iTextSharp c# library to split a pdf into seperate pages and rename them
     based on the pdf lables (page names), if existing. This library powers the copy of each page. The library can also
     split mult pages if necessary. 
 
-    Eventually the script will take into account Bookmarks (outlines) and identify any page numbers referenced. The script 
-    will then make a best guess at the page names based of user input reference of page number scheme. 
+    The script will take into account Bookmarks (outlines) and identify any page numbers referenced.This feature will be 
+    stronger the more pdfs i'm able to adjust too. The script will prefx 'Duplicate-' to any pages with no page reference
+    in the bookmarks. 
 
-    I used man References search the tag REF to find all links. 
+    I used many References, search the tag REF to find all links. 
+    REF:
     Source for library here: https://sourceforge.net/projects/itextsharp/
     Index for library: http://afterlogic.com/mailbee-net/docs-itextsharp/Index.html
 #>
-
-
-<#
-REF: https://www.codeproject.com/Articles/559380/Splitting-and-Merging-PDF-Files-in-Csharp-Using-iT
-#> #REF
+#REF: https://www.codeproject.com/Articles/559380/Splitting-and-Merging-PDF-Files-in-Csharp-Using-i
 
 Write-Host "Launching Split PDFs Utility...."
 
-#TODO: need to use $PSScriptroot to get root of current dir
-#############################################################################
-##This will need to reference the root folder of the location of the script##
-##The .dll and .xml for the itextsharp library will need to be in ~/lib/   ##
-#############################################################################
+#TODO: $use some kind of env variable to get the root of the folder
+#these needs to be set manually right now
 $dir = "\\Jester\Software\Miscellaneous\Scripts\SplitPDFs"
-#import dll
-Add-Type -Path $dir\lib\itextsharp.dll
 
-<#
-REF: https://www.codeproject.com/Articles/559380/Splitting-and-Merging-PDF-Files-in-Csharp-Using-iT
-#> #REF: for C#
+#REF: https://www.codeproject.com/Articles/559380/Splitting-and-Merging-PDF-Files-in-Csharp-Using-iT
+#REF: for C#
 Add-Type -ReferencedAssemblies "$dir\lib\itextsharp.dll" -typedefinition @"
 using iTextSharp.text;
 using iTextSharp.text.pdf;
@@ -177,19 +178,21 @@ Function get-Filename($initialDir) {
 
     $OpenFileDiag = New-Object System.Windows.Forms.OpenFileDialog
     $OpenFileDiag.InitialDirectory = $initialDir
-    $OpenFileDiag.Filter = "PDF (*.pdf)| *.pdf"
+    $OpenFileDiag.Filter = "PDF (*.pdf)| *.pdf" #limit to only PDFs
     $OpenFileDiag.ShowDialog() | Out-Null
     $OpenFileDiag.FileName
 }
-#window open initial location
+#save the path of the file
 $sourcePDFPath = get-Filename -initialDir $env:USERPROFILE\Desktop
+
+#import dll
+Add-Type -Path $dir\lib\itextsharp.dll
 
 #test document opening
 try{
     $document = New-Object iTextSharp.text.pdf.PdfReader -ArgumentList $sourcePDFPath -ErrorAction Stop
     $docTotalPages = $document.NumberOfPages
-    $arrayofLabels = [iTextSharp.text.pdf.PdfPageLabels]::GetPageLabels($document)
-    #$document.Close()
+    $arrayofLabels = [iTextSharp.Text.pdf.pdfpagelabels]::getpagelabels($document)
 }catch{
     Write-Host "Error importing PDF file"
     pause
@@ -197,33 +200,164 @@ try{
     #TODO: add error correction opportunity
 }
 
-#drive the script and the save off file names
-#do if when labels are for every page, otherwise do else
-if($arrayofLabels.Length -ne $docTotalPages){
+#Drive the script based off existing page lables or not
+#TODO: select method better
+if(($arrayofLabels.Length -ne $docTotalPages) -or ($arrayofLabels[0] -eq "1")){
+    #below will stop this if block if necessary.
+    #TODO: Add switch option of partial page labels and do same idea as bookmarks
+    #TODO: add ability to make do with partial of each (bookmarks and page labels)
+    <#
+    #go ahead an exit of no page labels are found
     if($arrayofLabels.length -eq 0){
         Write-Host "This PDF has no page labels! Exiting now"
         pause
         exit
     }
-    #need to adjust page numbers. Use chapters and incrememnt for each page in the chapter
-    #not enough label names in PDF
-    #TODO: Add what if no labels for each page
-    $page = 0
-    while($page -lt $docTotalPages){
-        if($arrayofLabels[$page] -eq $arrayofLabels[$page + 1]){
-            #check next label for the chapter 
+    #>
+    #REF: https://www.codeproject.com/tips/480622/extract-bookmark-from-pdf-file
+    ##########################################################################
+    #Set path labels and create folder
+    $folderName = [regex]::Split($sourcePDFPath,'.pdf$') #remove .pdf tag
+    $pdfName = %{$split = [regex]::Match($folderName,'[\-\w\.\ ]*$'); $split[0].Value} #select the name up to the \ in the path
+    $folderName = "Extracted-" + $pdfName
+    #create save to folder in root where sourcePDF is located called "Extracted-$pdfname"
+    [string]$destFolder = [regex]::Split($sourcePDFPath,'[\-\w\d\s]*.pdf$') -join $folderName
+    #TODO: why white space at end??
+    $destFolder =  [regex]::Replace($destFolder,'[\s]$','') #remove white space at end
+    #TODO: this will error if folder already exists unless error action exists
+    New-Item $destFolder -type Directory -ErrorAction SilentlyContinue
+    ##########################################################################
+    #memory stream for export XML
+    $ms = New-Object System.IO.MemoryStream
+    $arrayofBookmarks = [iTextSharp.text.pdf.SimpleBookmark]::GetBookmark($document)
+    [iTextSharp.text.pdf.SimpleBookmark]::ExportToXML($arrayofBookmarks,$ms,"ISO8859-1", $true)
+    $sr = New-Object System.IO.StreamReader($ms)
+    $ms.Position = 0
+    #exportXML Path to extracted folder to avoid confilcts with multi users
+    $outXML = "$destFolder\bookmarks.xml"
+    #export xml
+    $sr.ReadToEnd() | Out-File -FilePath $outXML
+    #import xml file
+    [xml]$inBookmarksXML = Get-Content -Path $outXML
+    #TODO try catch here to get errors if this doesn't work with other pdfs
+    ##########################################################################
+    #use the fact the result is a null value if no bookmarks are found to get to the correct bookmark depth and return that
+    if($inBookmarksXML.Bookmark.Title.Title.Title.Title -eq $null){
+        if($inBookmarksXML.Bookmark.Title.Title.Title -eq $null){
+            if($inBookmarksXML.Bookmark.Title.Title-eq $null){
+                if($inBookmarksXML.Bookmark.Title -eq $null){
+                    Write-Host "Bookmarks not found. Exiting Utility.."
+                    exit
+                }else{
+                    $listofBookmarks = $inBookmarksXML.Bookmark.Title
+                }
+            }else{
+                $listofBookmarks = $inBookmarksXML.Bookmark.Title.Title
+            }
+        }else{
+            $listofBookmarks = $inBookmarksXML.Bookmark.Title.Title.Title
         }
+    }else{
+        $listofBookmarks = $inBookmarksXML.Bookmark.Title.Title.Title.Title
     }
-
-}else{ #files are good to go to be named and saved
+    #make array from the XML formatted bookmark file
+    #$listofBookmarks = $inBookmarksXML.Bookmark.Title.Title.Title #this might not work on all pdfs..................
+    ##########################################################################
+    if($listofBookmarks.Count -gt $docTotalPages){
+        #More than necessary
+    }
+    #Process to get names of pages and numbers in order
+    #Initialize array slightly large, so each page is at the actualy page number
+    #TODO adjust to standard array not oversized one
+    $arraytotalpages = @($null) * ($docTotalPages+1)
+    #Go through what pages are known 
+    foreach($item in $listofBookmarks){
+        #Get page number from array list of bookmarks using REGEX
+        #Other items not number will be discarded
+        $index = [int][regex]::Match($item.Page, '\d*').Value
+        #make new object to house all the complete number of page names
+        $pdfPageObj = [PsCustomObject]@{
+            Page = [int][regex]::Match($item.Page, '\d*').Value
+            Title = [string]$item.'#text'
+         }
+         #assign accurate page to the array of complete pages. 
+         #i.e. page 3 from XML output is going to index #3 in this array
+         $arraytotalpages[$index] += $pdfPageObj
+    }
+    $index = 0
+    #set skipped pages
+    $guessedPages = @()
+    #Go through each looking for $null items (missed pages)
+    #Add any missing with a duplicate of the index's previous
+    $guessHappend = $false
+    foreach($item in $arraytotalpages){
+        #remember to skip the first as it will always be null technically
+        if(($item -eq $null) -and ($index -ne 0)){
+            #TODO: clean this write-host up when other output to user is cleaned up
+           Write-Host "Adding page at $index"
+           $guessHappend = $true
+           $pdfPageObj = [PsCustomObject]@{
+                Page = $index
+                Title = "DUPLICATE-" + $arraytotalpages[$index-1].Title
+            }
+            #make a guessed pages array to display to user later
+            $guessedPages += @($index)
+            $arraytotalpages[$index] = $pdfPageObj
+        }
+        $index++
+    }
+    $page = 1
+    #loop through all papes
+    while($page -le $docTotalPages){
+        Write-Progress -Activity "Spliting PDF... ['ctrl+c to' force stop]" -PercentComplete ($page / $docTotalPages * 100) -CurrentOperation "Page: $page of $docTotalPages."
+        #set save to dest
+        #Regex to remove disallowed characters from string of path
+        $pdfName = [regex]::Replace($arraytotalpages[$page].Title, '[^\w\-\.\ ]', '')
+        $savetoPath = $destFolder + "\" + $pdfName + ".pdf"
+        $newPDF = [iTextTools.PdfExtractorUtility]::new()
+         #count here so above path is accurate in getting label, and below is accurate on what page
+        try{
+            $newPDF.ExtractPage($sourcePDFPath, $savetoPath, $page)
+        }catch{
+            Write-Host "Error with page #$page" + $_.Exception.Message
+            pause
+            $document.Close()
+            exit
+        }
+        $page++
+    }
+    #remove the XML file created early
+    Remove-Item $outXML
+    #let user know if any items were guessed upon
+    #TODO get this cleaned up
+    if($guessHappend -eq $true){
+        Write-Host "There was not a complete list of bookmarks in this pdf. Guessed items will have the prefix 'Duplicate-'"
+        Write-Host "The pages without accurate bookmarks are" -NoNewline  
+        foreach($page in $guessedPages) {Write-host " " $page -NoNewline}
+    }
+    Write-Host ''
+    Write-Host "Extracted files are located in $destFolder"
+    #Close files and streams
+    $document.Close()
+    $sr.Close()
+    $ms.Close()
+    ########################
+    ####end if, now else####
+    ########################
+    }else{ 
+    #files have labels for each page
+    ##########################################################################
     #Set path labels
     $folderName = [regex]::Split($sourcePDFPath,'.pdf$') #remove .pdf tag
-    $folderName = %{$split = [regex]::Match($folderName,'[\w\.\ ]*$'); $split[0].Value} #select the name up to the \ in the path right to left
-    $folderName = [regex]::Split($folderName,'[\s]$') -join "-Extracted" #remove white space at end (for seme reason left) probably regex on line 219
-    #create save to folder in root where sourcePDF is located called "$pdfname-Extracted"
-    #TODO: instead of regex for name of PDF use $document.
-    $destFolder = [regex]::Split($sourcePDFPath,'[\w\d\s]*.pdf$') -join $folderName 
+    $pdfName = %{$split = [regex]::Match($folderName,'[\-\w\.\ ]*$'); $split[0].Value} #select the name up to the \ in the path
+    #$pdfName =  [regex]::Split($pdfName,'[\s]$') #remove white space at end of pdf name
+    $folderName = "Extracted-" + $pdfName
+    #$folderName =  [regex]::Split($folderName,'[\s]$') #remove white space at end 
+    #create save to folder in root where sourcePDF is located called "Extracted"
+    [string]$destFolder = [regex]::Split($sourcePDFPath,'[\-\w\d\s]*.pdf$') -join $folderName
+    $destFolder =  [regex]::Replace($destFolder,'[\s]$','') #remove white space at end
     New-Item $destFolder -type Directory
+    ##########################################################################
     $page = 0
     #loop through all papes
     while($page -lt $docTotalPages){
@@ -233,14 +367,17 @@ if($arrayofLabels.Length -ne $docTotalPages){
         $pdfName = [regex]::Replace($arrayofLabels[$page], '[^\w\-\.\ ]', '')
         $savetoPath = $destFolder + "\" + $pdfName + ".pdf"
         $newPDF = [iTextTools.PdfExtractorUtility]::new()
-        $page++ #count here so above path is accurate in getting label, and below is accurate on what page
+        #count here so above path is accurate in getting label, and below is accurate on what page
+        $page++ 
         try{
             $newPDF.ExtractPage($sourcePDFPath, $savetoPath, $page)
         }catch{
-            Write-Host "Error with page #$page." + $_.Exception.Message
+            Write-Host "Error with page #$page" + $_.Exception.Message
             pause
             $document.Close()
             exit
         }
     }
-}
+    Write-Host "Splitting PDF has finished. PDF page labels were used to name the files."
+    Write-Host "Extracted files are located in $destFolder"
+  }
